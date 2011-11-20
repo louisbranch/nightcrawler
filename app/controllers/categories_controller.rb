b09e382eabfa10f74651e6b94c64987bc17bcaf2
Luiz Branco <artfikan@gmail.com>
2011-11-20 13:37:46 -0200

    edit form fix, story sort order and hide empty categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   
   def show
     @category = Category.find(params[:id])
-    @stories = @category.stories.page(params[:pagina]).per_page(10)
+    @stories = @category.stories.page(params[:pagina]).order('created_at DESC').per_page(10)
   end
   
 end
